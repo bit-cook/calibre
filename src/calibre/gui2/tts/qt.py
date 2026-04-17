cff5a50b202d051aa0dae1807a99c0b4223d4383
Kovid Goyal <kovid@kovidgoyal.net>
2026-04-17 08:46:10 +0530

    ...

--- src/calibre/gui2/tts/qt.py
+++ src/calibre/gui2/tts/qt.py
@@ -115,7 +115,7 @@ class QtTTSBackend(TTSBackend):
         self._current_settings = settings
 
     def _saying_word(self, word: str, utterance_id: int, start: int, length: int) -> None:
-        print(f'{repr(word)=} {start=} {length=}, {repr(self.speaking_text[start:start+length])=} {self.ignore_tracking_until_state_changes_to_speaking=}')
+        # print(f'{repr(word)=} {start=} {length=}, {repr(self.speaking_text[start:start+length])=} {self.ignore_tracking_until_state_changes_to_speaking=}')
         if self.ignore_tracking_until_state_changes_to_speaking:
             return
         # Qt's word tracking is broken with non-BMP unicode chars, the
@@ -126,7 +126,7 @@ class QtTTSBackend(TTSBackend):
             return
         self.last_spoken_word = key
         idx = self.speaking_text.find(word, self.last_word_offset)
-        print(f'{self.last_word_offset=} {idx=}')
+        # print(f'{self.last_word_offset=} {idx=}')
         if idx > -1:
             self.saying.emit(idx, len(word))
             self.last_word_offset = idx + len(word)
